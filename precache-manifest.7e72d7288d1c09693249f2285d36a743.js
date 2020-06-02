10e6b8c2f104143d7dfbe057bb5dc2550b1da505
Christian <Christianwdelcid@gmail.com>
2020-06-01 20:35:16 -0400

    Updates

---
+++ precache-manifest.7e72d7288d1c09693249f2285d36a743.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8966246bf70876ef3d871c84acf38c44",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8f0cc6707e91aa4c21a0",
+    "url": "/weather-app/static/js/2.a3a900b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.a3a900b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fcf7a8f57911ae057e",
+    "url": "/weather-app/static/js/main.3539858a.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  },
+  {
+    "revision": "c634cf8ac6d148033eb1b2759979b2c9",
+    "url": "/weather-app/static/media/clear-skies_day2.c634cf8a.jpg"
+  },
+  {
+    "revision": "ffcbb69d3e335e319bcb81c36a60a14c",
+    "url": "/weather-app/static/media/clear-skies_night.ffcbb69d.jpg"
+  },
+  {
+    "revision": "c18ee514e03915041fb9a4762518a9f8",
+    "url": "/weather-app/static/media/clouds_day2.c18ee514.jpg"
+  },
+  {
+    "revision": "88416c5188d7786f5056288cc60ad6d3",
+    "url": "/weather-app/static/media/clouds_night.88416c51.jpg"
+  },
+  {
+    "revision": "9fa41c2678bb3460e4720245708f7acf",
+    "url": "/weather-app/static/media/haze_day.9fa41c26.jpg"
+  },
+  {
+    "revision": "6f07f41878f93f78882e8cd942a41855",
+    "url": "/weather-app/static/media/haze_night.6f07f418.jpg"
+  },
+  {
+    "revision": "afe7a45331971799ce8a760b19488f67",
+    "url": "/weather-app/static/media/rain_day2.afe7a453.jpg"
+  },
+  {
+    "revision": "cdb89ab6c5ced087964d2671bd84177e",
+    "url": "/weather-app/static/media/rain_night3.cdb89ab6.jpg"
+  },
+  {
+    "revision": "3f1da173863b747a1376c2f4b5351f44",
+    "url": "/weather-app/static/media/snow_day2.3f1da173.jpg"
+  },
+  {
+    "revision": "20c8452cecc19637655e77483511ef6f",
+    "url": "/weather-app/static/media/snow_night.20c8452c.jpg"
+  },
+  {
+    "revision": "c2a3c37a5b8b8ef6df6c44c3f9d5cd15",
+    "url": "/weather-app/static/media/standard.c2a3c37a.jpg"
+  },
+  {
+    "revision": "18ddbfd49b67615c158465e2e4a331d5",
+    "url": "/weather-app/static/media/thunder_day.18ddbfd4.jpg"
+  },
+  {
+    "revision": "1e9824ac8999c14cd9b560ceb9ffa9e7",
+    "url": "/weather-app/static/media/thunder_night.1e9824ac.jpg"
+  }
+]);
